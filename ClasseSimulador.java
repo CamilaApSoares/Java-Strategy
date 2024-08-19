6c192c3014905db92a6a7f41bf95f33a27faa815
Camila Aparecida <114444962+CamilaApSoares@users.noreply.github.com>
2024-08-19 17:13:25 -0300

    Create ClasseSimulador.java

---
+++ ClasseSimulador.java
@@ -0,0 +1,16 @@
+package teste;
+
+public class Simulador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		grupoSobrevivente gruposobrevivente1 = new grupoSobrevivente("Zumami", 26, true);
+		grupoSobrevivente gruposobrevivente2 = new grupoSobrevivente("Bardurs", 30, false);
+
+		
+		
+		}
+	}
+
+}
+
