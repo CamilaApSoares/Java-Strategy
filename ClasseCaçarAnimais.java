b4bcd190c5e847f2f4d8a3afb48ac0137c7862ae
Camila Aparecida <114444962+CamilaApSoares@users.noreply.github.com>
2024-08-21 13:32:57 -0300

    Update ClasseCaçarAnimais.java

--- ClasseCaçarAnimais.java
+++ ClasseCaçarAnimais.java
@@ -2,12 +2,12 @@ package teste;
 
 public class CacarAnimais implements EstrategiaSobrevivencia{
 	 @Override
-	 public void enfrentarPerigo(Grupo Inimigo){
+	 public void enfrentarPerigo(grupoSobrevivente inimigo){
 		 System.out.println("\n Atacar familia de jacarés \n Atacar grupo de galinhas");
 	 }
 	 
 	 @Override
-	 public void finalizar(){
+	 public void finalizar(grupoSobrevivente inimigo){
 		 System.out.println("\n Levar a caça para o grupo");
 	 }
 }
