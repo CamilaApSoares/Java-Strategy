6a2b6db5af976728dfd56ba0960846366a7c2a58
Camila Aparecida <114444962+CamilaApSoares@users.noreply.github.com>
2024-08-21 13:30:25 -0300

    Update ClasseSimulador.java

--- ClasseSimulador.java
+++ ClasseSimulador.java
@@ -4,11 +4,13 @@ public class Simulador {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		grupoSobrevivente gruposobrevivente1 = new grupoSobrevivente("Zumami", 26, true);
+		grupoSobrevivente gruposobrevivente1 = new grupoSobrevivente("Zumami", 90, true);
 		grupoSobrevivente gruposobrevivente2 = new grupoSobrevivente("Bardurs", 30, false);
 
-		
-		
+		if(gruposobrevivente1.getQtdMembros() < gruposobrevivente2.getQtdMembros()){
+			System.out.println("grupo um ganhou");
+		}else{
+			System.out.println("Gurpo 2 ganhou");
 		}
 	}
 
