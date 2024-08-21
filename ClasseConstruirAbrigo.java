61aa6beb5b863cadd732b942bb470e7fbfd7e2cd
Camila Aparecida <114444962+CamilaApSoares@users.noreply.github.com>
2024-08-21 13:31:36 -0300

    Update ClasseConstruirAbrigo.java

--- ClasseConstruirAbrigo.java
+++ ClasseConstruirAbrigo.java
@@ -2,12 +2,12 @@ package teste;
 
 public abstract class ConstruirAbrigo implements EstrategiaSobrevivencia{
 	@Override
-	public void enfrentarPerigo(Grupo Inimigo){
+	public void enfrentarPerigo(grupoSobrevivente Inimigo){
 		System.out.println("\n Cavar em uma terra fofa com perigo de desabamento");
 	}
 
 	@Override
-	public void finalizar(Grupo Inimigo){
+	public void finalizar(grupoSobrevivente Inimigo){
 		System.out.println("\n Concluir com sucesso a construção do abrigo");
 	}
 	
